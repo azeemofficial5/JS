405360d66e6001f5217c3941a84fd801b95cbbf4
azeemofficial5 <138965361+azeemofficial5@users.noreply.github.com>
2023-08-28 02:54:13 +0000

    container confrig done

---
+++ 01_besics/01_variable.js
@@ -0,0 +1,21 @@
+const accountId = 786
+let emailId = "azeem@.com"
+var password = "1212"
+accountCity ="Rampur"
+// accountId =121212// not allowed
+let accountState;
+console.log(accountCity);
+
+/*
+Prefer not to use var
+bacause of issue in block scope and Functional scope
+*/
+
+let EmailId="sarfaraz@.com"
+let PasswordId="2312"
+AccountCity="moradabad"
+
+console.table([accountId,EmailId, PasswordId, AccountCity, accountState])
+
+
+
